452f072ee76605723131f73f3452ca9c0c748bca
AntoniaG <antonia.grillmair@gmail.com>
2020-01-09 14:27:19 +0100

    first

---
+++ src/main/java/com/example/demo/Customer.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class Customer {
+
+    private String f;
+    private String l;
+
+    public String getF() {
+        return f;
+    }
+
+    public void setF(String f) {
+        this.f = f;
+    }
+
+    public String getL() {
+        return l;
+    }
+
+    public void setL(String l) {
+        this.l = l;
+    }
+
+}
