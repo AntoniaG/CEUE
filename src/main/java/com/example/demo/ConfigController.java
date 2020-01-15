8c80227392cfef0d5bba64d2bf036d963d0d9e57
AntoniaG <antonia.grillmair@gmail.com>
2020-01-15 21:35:41 +0100

    ERP

--- src/main/java/com/example/demo/ConfigController.java
+++ src/main/java/com/example/demo/ConfigController.java
@@ -1,7 +1,24 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+
+@Controller
 public class ConfigController {
 
+    @GetMapping ("/handlebar")
+    public String config(Model model){
+        System.out.println("hello world");
+        RestTemplate restTemplate = new RestTemplate();
+
+        model.addAttribute("hbTypes", restTemplate.getForObject(
+                "https://www.maripavi.at/produkt/lenkertyp", String[].class));
+        return "handlebar";
+    }
 
 
 }
--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -20,6 +23,15 @@ public class CustomerController {
     public String customerSubmit(@ModelAttribute Customer customer) {
         return "result";
     }
+
+    /*
+        @PostMapping
+        public ResponseEntity<Customer> customer(@RequestParam Customer customer) {
+        customer.setF();
+        customer.setL();
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+     */
 
 }
 
